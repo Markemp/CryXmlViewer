1f238ed7dfe5eae4208a7506a0fe324c53119d78
Heffay <ggerber@gmail.com>
2023-12-27 14:15:14 -0700

    Format extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,29 +4,35 @@ import * as fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
-      "cryxmlviewer.openBinaryXml",
-      async (uri: vscode.Uri) => {
-          try {
-              const fileBuffer = fs.readFileSync(uri.fsPath);
-              if (CryXmlSerializer.isBinaryXml(fileBuffer)) {
-                  const xmlContent = await CryXmlSerializer.readFile(fileBuffer);
+    "cryxmlviewer.openBinaryXml",
+    async (uri: vscode.Uri) => {
+      try {
+        const fileBuffer = fs.readFileSync(uri.fsPath);
+        if (CryXmlSerializer.isBinaryXml(fileBuffer)) {
+          const xmlContent = await CryXmlSerializer.readFile(fileBuffer);
 
-                  const xmlDocument = await vscode.workspace.openTextDocument({
-                      language: "xml",
-                      content: xmlContent
-                  });
-                  await vscode.window.showTextDocument(xmlDocument, { preview: false });
-              } else {
-                  vscode.window.showErrorMessage("File is not a recognized binary XML format.");
-              }
-          } catch (error) {
-              if (error instanceof Error) {
-                  vscode.window.showErrorMessage(`Error processing file: ${error.message}`);
-              } else {
-                  vscode.window.showErrorMessage("Error processing file: An unknown error occurred");
-              }
-          }
+          const xmlDocument = await vscode.workspace.openTextDocument({
+            language: "xml",
+            content: xmlContent,
+          });
+          await vscode.window.showTextDocument(xmlDocument, { preview: false });
+        } else {
+          vscode.window.showErrorMessage(
+            "File is not a recognized binary XML format."
+          );
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          vscode.window.showErrorMessage(
+            `Error processing file: ${error.message}`
+          );
+        } else {
+          vscode.window.showErrorMessage(
+            "Error processing file: An unknown error occurred"
+          );
+        }
       }
+    }
   );
 
   context.subscriptions.push(disposable);
